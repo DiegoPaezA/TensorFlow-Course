a502806cf16ecedbe219398b4cc17ebd115bc797
diripar8 <diripar8@gmail.com>
2023-01-26 13:09:23 -0500

    new functions unzip and results

--- helper_functions.py
+++ helper_functions.py
@@ -8,6 +8,8 @@ Las funciones son:
 - pred_and_plot: para hacer una predicción y graficar la imagen
 - create_tensorboard_callback: para crear un callback para TensorBoard
 - walk_through_dir: para visualizar las imágenes de un directorio
+- Función para descomprimir un dataset
+- Función para calcular el resultado del desempeño de un modelo
 """
 
 import matplotlib.pyplot as plt
@@ -17,7 +19,8 @@ import os
 import tensorflow as tf
 import numpy as np
 import datetime
-
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import zipfile
 
 
 """"
@@ -221,3 +224,41 @@ def walk_through_dir(dir_path):
   """
   for dirpath, dirnames, filenames in os.walk(dir_path):
     print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+"""
+Función para descomprimir un dataset
+"""
+def unzip_data(filename):
+  """
+  Unzips filename into the current working directory.
+
+  Args:
+    filename (str): a filepath to a target zip folder to be unzipped.
+  """
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
+
+
+"""
+Función para calcular el resultado del desempeño de un modelo
+"""
+def calculate_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+      y_true: true labels in the form of a 1D array
+      y_pred: predicted labels in the form of a 1D array
+
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  # Calculate model precision, recall and f1 score using "weighted average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+  return model_results
